a333de7d35de0b6be1e5ad55c9a6a5fd0dd4e71a
Ivan Bogdan <bogdan.ivan@nsoft.com>
2020-10-02 18:04:01 +0200

    Peto predavanje nastavak

--- 05_predavanje/main.js
+++ 05_predavanje/main.js
@@ -5,55 +5,56 @@
   Second comment type - multiline
 */
 
-
 /* Deklaracija */
-console.log('Declaration:');
+console.log("Declaration:");
 var apples;
 var x;
 var y;
 var someString;
 
-console.log('Before init: someString =>', someString); // undefined
+console.log("Before init: someString =>", someString); // undefined
 
 /* Inicijalizacija */
-console.log('Initialization:');
+console.log("Initialization:");
 apples = 3;
 x = 5;
 y = 10;
-someString = 'Neki tamo tekst!';
+someString = "Neki tamo tekst!";
 
-console.log('After init: someString =>', someString); // undefined
+console.log("After init: someString =>", someString); // undefined
 
 /* Deklaracija i inicijalizacija */
-console.log('Declaration with initialization:');
-var z = 'Boy has ' + apples + ' apples.';
+console.log("Declaration with initialization:");
+var z = "Boy has " + apples + " apples.";
 var counter = 0;
 
-console.log('Displaying multiple variables:', z, counter);
+console.log("Displaying multiple variables:", z, counter);
 
-var a = 'a',
-    b = 'TEKSTINA.',
-    c = 100000;
+var a = "a",
+  b = "TEKSTINA.",
+  c = 100000;
 
 /* untyped language */
 
 var typeUndefined = undefined,
-    typeNull = null,
-    typeBoolFalse = false,
-    typeBoolTrue = true,
-    typeInteger = 1,
-    typeString = 'Tekst',
-    typeFunction = function () {},
-    typeObject = {},
-    typeArray = [],
-    typeRegexp = /abc/g;
+  typeNull = null,
+  typeBoolFalse = false,
+  typeBoolTrue = true,
+  typeInteger = 1,
+  typeString = "Tekst",
+  typeFunction = function () {},
+  typeObject = {},
+  typeArray = [],
+  typeRegexp = /abc/g;
 
-console.log('===============================================================================================');
+console.log(
+  "==============================================================================================="
+);
 
 /* Global scope */
 console.log(window);
 
-var someRandomVar = 'window';
+var someRandomVar = "window";
 
 function funcA() {
   /* Local scope of funcA */
@@ -64,12 +65,12 @@ function funcA() {
   */
   console.log(a, b, c);
 
-  var someRandomVar = 'funcA';
+  var someRandomVar = "funcA";
   console.log("funcA -> someRandomVar", someRandomVar);
 }
 
 funcA(); // Poziv funkcije
-console.log(someRandomVar); 
+console.log(someRandomVar);
 
 /* Naming of the variables */
 
@@ -86,50 +87,154 @@ THIS IS NOT OK. It will throw error.
 */
 
 // THIS IS OK.
-var _123x,
-    _variable_02;
+var _123x, _variable_02;
 
 // Two separate variables. JS is case-sensitive.
 
 var name, Name;
 
-console.log('===============================================================================================');
+console.log(
+  "==============================================================================================="
+);
 
 var _x = 5,
-    _y = 3;
+  _y = 3;
 
-console.log('var _x = 5,\n    _y = 3;')
-console.log('_x + _y =', _x + _y);
-console.log('_x - _y =', _x - _y);
-console.log('_x * _y =', _x * _y);
-console.log('_x / _y =', _x / _y);
-console.log('_x % _y =', _x % _y, 'Info: calculates the remain of two number division. We call it "modulo".');
-console.log('_x++ =', _x++, ' SAME AS ', '_x = _x + 1', '_x += 1');
-console.log('_x-- =', _x--, ' SAME AS ', '_x = _x - 1', '_x -= 1');
-console.log('_x *= _y', _x *= _y, ' SAME AS ', '_x = _x * _y');
-console.log('_x /= _y', _x /= _y, ' SAME AS ', '_x = _x / _y');
-console.log('_x %= _y', _x %= _y, ' SAME AS ', '_x = _x % _y');
+console.log("var _x = 5,\n    _y = 3;");
+console.log("_x + _y =", _x + _y);
+console.log("_x - _y =", _x - _y);
+console.log("_x * _y =", _x * _y);
+console.log("_x / _y =", _x / _y);
+console.log(
+  "_x % _y =",
+  _x % _y,
+  'Info: calculates the remain of two number division. We call it "modulo".'
+);
+console.log("_x++ =", _x++, " SAME AS ", "_x = _x + 1", "_x += 1");
+console.log("_x-- =", _x--, " SAME AS ", "_x = _x - 1", "_x -= 1");
+console.log("_x *= _y", (_x *= _y), " SAME AS ", "_x = _x * _y");
+console.log("_x /= _y", (_x /= _y), " SAME AS ", "_x = _x / _y");
+console.log("_x %= _y", (_x %= _y), " SAME AS ", "_x = _x % _y");
 
 var _A = 5,
-    _B = 7;
+  _B = 7;
 
-console.log('var _A = 5,\n    _B = 7;')
-console.log('_A == _B', _A == _B);
-console.log('_A != _B', _A != _B);
-console.log('_A > _B', _A > _B);
-console.log('_A < _B', _A < _B);
-console.log('_A >= _B', _A >= _B);
-console.log('_A <= _B', _A <= _B);
-console.log('_A == _B', _A == _B);
-console.log('_A == _B', _A == _B);
+console.log("var _A = 5,\n    _B = 7;");
+console.log("_A == _B", _A == _B);
+console.log("_A != _B", _A != _B);
+console.log("_A > _B", _A > _B);
+console.log("_A < _B", _A < _B);
+console.log("_A >= _B", _A >= _B);
+console.log("_A <= _B", _A <= _B);
+console.log("_A == _B", _A == _B);
+console.log("_A == _B", _A == _B);
 
-console.log('PREPORUKA (mora slijediti): UVIJEK KORISTITI 3 znaka jednakosti ===, umjesto 2 znaka ==.')
+console.log(
+  "PREPORUKA (mora slijediti): UVIJEK KORISTITI 3 znaka jednakosti ===, umjesto 2 znaka ==."
+);
 
 console.log('5 == "5"', 5 == "5");
 console.log('5 === "5"', 5 === "5");
 console.log('5 != "5"', 5 != "5");
 console.log('5 !== "5"', 5 !== "5");
 
+console.log(
+  "==============================================================================================="
+);
+console.log("&& LOGIČKI OPERATOR:");
+console.log("false && false => ", false && false);
+console.log("true && false => ", true && false);
+console.log("false && true => ", false && true);
+console.log("true && true => ", true && true);
+
+console.log("NOTE: !false => ", !false, "!true => ", !true);
+
+console.log("!(boolean && boolean) LOGIČKI OPERATOR:");
+console.log("!(false && false) => ", !(false && false));
+console.log("!(true && false) => ", !(true && false));
+console.log("!(false && true) => ", !(false && true));
+console.log("!(true && true) => ", !(true && true));
+
+console.log("!boolean && !boolean LOGIČKI OPERATOR:");
+console.log("!false && !false => ", !false && !false);
+console.log("!true && !false => ", !true && !false);
+console.log("!false && !true => ", !false && !true);
+console.log("!true && !true => ", !true && !true);
+
+console.log(
+  "==============================================================================================="
+);
+
+console.log("|| LOGIČKI OPERATOR:");
+console.log("false || false => ", false || false);
+console.log("true || false => ", true || false);
+console.log("false || true => ", false || true);
+console.log("true || true => ", true || true);
+
+console.log("NOTE: !false => ", !false, "!true => ", !true);
+
+console.log("!(boolean || boolean) LOGIČKI OPERATOR:");
+console.log("!(false || false) => ", !(false || false));
+console.log("!(true || false) => ", !(true || false));
+console.log("!(false || true) => ", !(false || true));
+console.log("!(true || true) => ", !(true || true));
+
+console.log("!boolean || boolean LOGIČKI OPERATOR:");
+console.log("!false || !false => ", !false || !false);
+console.log("!true || !false => ", !true || !false);
+console.log("!false || !true => ", !false || !true);
+console.log("!true || !true => ", !true || !true);
+
+console.log("===============================================================================================");
+
+console.log("true && true || false && true => ", true && true || false && true);
+
+console.log(false && true || true && true);
+console.log("2. true || false || false && true => ", true || false || false && true);
+console.log("3. false && false && true => ", false && false && true);
+
+console.log("===============================================================================================");
+
+/* 
+  Truthy: 
+    * true
+    * 'neki tamo string'
+    * 'false' 
+    * []
+    * ['string']
+    * {}
+    * 1
+    * 56
+
+  Falsy: 
+    * false
+    * 0
+    * ''
+    * undefined
+    * null
+    * NaN (NotANumber)
+*/
+
+console.log("1. {} && 0 || 45 && ['string'] && NaN =>");
+console.log("Equivalent => true && false || true && true && false => false");
+
+console.log("2. null || [] || 'false' && 1 => true", null || [] || 'false' && 1);
+console.log("3. { user: 'user' } && '' || true => true", { user: 'user' } && '' || true);
+console.log("4. false && (undefined || 'false') && !!{} => false", false && (undefined || 'false') && !!{});
+
+console.log("Kako bi prebacili truthy i falsy vrijednosti u njihov boolean ekvivalent koristimo !! sa lijeve strane => !!null === false, !![] === true")
+
+console.log("Vrijednosti grupiramo pomocu !!((true && false || true) && true)");
+
+console.log("===============================================================================================");
+
+console.log("typeof 2 => ", typeof 2);
+console.log("typeof '2' => ", typeof '2');
+console.log("typeof [2] => ", typeof [2]);
+console.log("typeof {} => ", typeof {});
+console.log("typeof function() {} => ", typeof function() {});
+console.log("typeof null => ", typeof null);
+console.log("typeof undefined => ", typeof undefined);
 
 /* 
 HOW TO CUSTOMIZE EDITOR:
@@ -143,44 +248,20 @@ HOW TO CUSTOMIZE EDITOR:
   }
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /* Etc */
 
 /* Real life examples: */
 
 function giveAnAppleToTheKid() {
-  console.log('Kid recieves one apple.');
+  console.log("Kid recieves one apple.");
   apples = apples + 1; // apple++;
-  console.log('Boy has ' + apples + ' apples.');
-  document.getElementById('apple-count').textContent = apples;
+  console.log("Boy has " + apples + " apples.");
+  document.getElementById("apple-count").textContent = apples;
 }
 
 function takeAnAppleFromTheKid() {
-  console.log('Kid loses an apple.');
+  console.log("Kid loses an apple.");
   apples--; // apple = apple - 1;
-  console.log('Boy has ' + apples + ' apples.');
-  document.getElementById('apple-count').textContent = apples;
+  console.log("Boy has " + apples + " apples.");
+  document.getElementById("apple-count").textContent = apples;
 }
-
---
+++ 06_predavanje/main.js
@@ -0,0 +1,89 @@
+function changeElementBackground (hexColor) {
+  if (hexColor === '#00FF00') {
+    console.log("SUCCESS!");
+  } else {
+    console.log("COLOR!");
+  }
+
+  if (hexColor === '#FF0000') return;
+
+  var div = document.createElement('div');
+  div.innerText = 'NEKI TAMO TEKST';
+  div.style.backgroundColor = hexColor;
+
+  var body = document.getElementsByTagName('body')[0];
+  body.insertBefore(div, body.lastElementChild);
+};
+
+function addToSum(value) {
+  var sumElement = document.getElementById('sum'),
+      currentSum = parseInt(sumElement.innerText),
+      parsedValue = parseInt(value);
+
+  if (value === "Reset") { // Vrijednost mora biti tocno "Reset" 
+    sumElement.innerText = 0;
+  } else if (parsedValue >= 1 && parsedValue <= 25) { // Vrijednost mora biti veca ili jednaka od 1 i manja ili jednaka od 25 (1 - 25);
+    console.log("Good job for betting rationally.");
+    sumElement.innerText = currentSum + parsedValue;
+  } else if (parsedValue > 25 && parsedValue <= 100) { // Vrijednost mora biti veca od 25 i manja ili jednaka od 100 (1 - 100);
+    console.log("Not sure if that's a good idea, but go for it.");
+    sumElement.innerText = currentSum + parsedValue;
+  } else if (parsedValue > 100 && parsedValue <= 250) { // Vrijednost mora biti veca od 100 i manja ili jednaka od 250 (1 - 250);
+    console.log("Hold it right there champ!");
+  } else if (parsedValue === 500) { // Vrijednost mora tocno 500 (i tip i vrijednost, i.e. '500' ne bi radilo);
+    console.log("It's time to leave sir.")
+  } else { // U slucaju da niti jedan od gornjih uvjeta nije zadovoljen izvrsi ovo.
+    console.log("Something went wrong with the amount you wanted to insert. VALUE: ", parsedValue);
+  }
+}
+
+var items = [];
+
+function checkValue(value) {
+  switch (value) {
+    case 'Box':
+      console.log("You've added a box");
+      break;
+    case 'Card':
+      console.log("You've added a card");
+      break;
+    case 'Bag':
+      console.log("You've added a bag");
+      break;
+    case 'Cup':
+      console.log("You've added a cup");
+      break;
+    case 'Bottle':
+      console.log("You've added a bottle");
+      break;
+    default: 
+      console.log("The item you entered is invalid");
+  }
+}
+
+function checkValueOptimized (value) {
+  console.log("You've added a " + value);
+  items.push(value);
+  console.log(items);
+}
+
+/* 
+  if (value === 'Box') {
+    console.log("You've added a box");
+    items.push(value);
+  } else if (value === 'Card') {
+    console.log("You've added a card");
+    items.push(value);
+  } else if (value === 'Bag') {
+    console.log("You've added a bag");
+    items.push(value);
+  } else if (value === 'Cup') {
+    console.log("You've added a cup");
+    items.push(value);
+  } else if (value === 'Bottle') {
+    console.log("You've added a bottle");
+    items.push(value);
+  } else {
+    console.log("The item you entered is invalid");
+  }
+*/
